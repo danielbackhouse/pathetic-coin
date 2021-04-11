d93f56fe66f48a1c251c856f89826967ab0843c3
Daniel Backhouse <daniel.backhouse@alumni.ubc.ca>
2021-04-10 20:35:57 -0700

    Finished utils adding hashing and sorting functionaility.
    Finished utils hashing and sorting functionality. Testing is working for this section.

--- util/index.js
+++ util/index.js
@@ -1,8 +1,23 @@
+const keccak256 = require('js-sha3').keccak256;
+
 const sortCharacters = (data) => {
-    return JSON.stringify(data).split('').sort().join('')
+    return JSON.stringify(data).split('').sort().join('');
+}
+
+const keccakHash = (data) => {
+    const hash = keccak256.create();
+
+    // Important to note here that we are sorting the characters so 
+    // that the data we pass to the hash which the properties arent in the same order for the ojbectcan
+    // be out of order but must just contain the same information
+    // not ideal.
+    hash.update(sortCharacters(data));
+
+    return hash.hex()
 }
 
 
 module.exports = {
-    sortCharacters
+    sortCharacters,
+    keccakHash
 };
